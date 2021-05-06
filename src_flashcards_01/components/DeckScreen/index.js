cd540bb43668fa09faa9574c6456059b84ab0448
JadneDream <brady0416@gmail.com>
2021-05-06 11:33:54 +0800

    Update index.js
    清理

--- src_flashcards_01/components/DeckScreen/index.js
+++ src_flashcards_01/components/DeckScreen/index.js
@@ -3,12 +3,9 @@ import { View } from "react-native";
 import { addDeck } from "./../../actions/creators";
 import DeckCreation from "./DeckCreation";
 import DeckList  from './DeckList'
-import Button from "./../Button";
-import NormalText from "./../NormalText";
 
 class DecksScreen extends Component {
   static displayName = "DecksScreen";
-
   static navigationOptions = { title: "All Decks" };
 
   _createDeck = name => {
@@ -22,41 +19,11 @@ class DecksScreen extends Component {
   render() {
     return (
       <View>
-        <Button onPress={() => this.props.navigation.push('Review')}>
-        <NormalText>
-          cards
-        </NormalText>
-        </Button>
         <DeckList />
         <DeckCreation create={this._createDeck} />
       </View>
     );
   }
 }
-
-/*
-const mapDispatchToProps = dispatch => {
-  return {
-    createDeck: deckAction => {
-      dispatch(deckAction);
-    },
-    reviewDeck: deckID => {
-      dispatch(reviewDeck(deckID));
-    }
-  };
-};
-
-const mapStateToProps = state => {
-  return {
-    decks: state.decks,
-    counts: state.decks.reduce(
-      (sum, deck) => {
-        sum[deck.id] = deck.cards.length;
-        return sum;
-      },
-      {}
-    )
-  };
-};*/
 
 export default DecksScreen;
